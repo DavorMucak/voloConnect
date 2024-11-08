c38c5aea7df37e569d7261cf6930742eff786e44
Ena Dodig <ena.dodig@fer.hr>
2024-11-08 13:08:23 +0100

    dropdown uloga

---
+++ login-register_frontend/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div class="mini-container">
+      <button @click="isLogin = true">Prijava</button>
+      <button @click="isLogin = false">Registracija</button>
+    </div>
+    <div v-if="isLogin">
+      <Login />
+    </div>
+    <div v-else>
+      <Register />
+    </div>
+  </div>
+</template>
+
+<script>
+import Login from './components/Login.vue';
+import Register from './components/Register.vue';
+
+export default {
+  components: {
+    Login,
+    Register,
+  },
+  data() {
+    return {
+      isLogin: false, // register forma je default
+    };
+  },
+};
+</script>
---
+++ login-register_frontend/src/components/Login.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <h2>Prijava</h2>
+
+        <form @submit.prevent="login">
+            <input type="text" v-model="username" placeholder="korisničko ime" />
+            <input type="password" v-model="password" placeholder="lozinka" />
+
+            <select v-model="selected" class="selection">
+                <option disabled value="">uloga</option>
+                <option v-for="option in options" :key="option" :value="option">
+                    {{ option }}
+                </option>
+            </select>
+
+            <button type="submit">Prijava</button>
+            <p class="error" v-if="error">{{ error }}</p>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: '',
+            password: '',
+            error: '',
+            selected: '', 
+            options: ["volonter", "organizacija", "admin"],
+        };
+    },
+    methods: {
+        login() {
+            this.error = '';  //resetira error poruke na default(blank)
+
+            if (!this.username || !this.password || !this.selected) {
+                this.error = 'Molimo unesite korisničko ime i lozinku.';
+                return;
+            }
+
+            if (this.username === 'user' && this.password === 'sifra') {
+                alert('Uspješna prijava');
+            } else {
+                this.error = 'Pogrešno korisničko ime ili lozinka.';
+            }
+        },
+    },
+};
+</script>
