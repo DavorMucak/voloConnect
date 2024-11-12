9c581676a5b568b1577f12ebc99516c253357768
unknown <dm49143@fer.hr>
2024-11-12 17:36:33 +0100

    base prepravljen da podržava relativne putanje

--- login-register_frontend/vite.config.js
+++ login-register_frontend/vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: '', // Postavlja relativnu putanju za sve resurse
   plugins: [
     vue(),
   ],
